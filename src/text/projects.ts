098386131086288f759f2264d3c9591f8739a3c6
hanif <hanifsholihin884@gmail.com>
2026-01-05 11:14:52 +0700

    feat add new project

--- src/text/projects.ts
+++ src/text/projects.ts
@@ -10,6 +10,10 @@ import adminai from "@/src/public/adminai (1).png";
 import adminai2 from "@/src/public/adminai (2).png";
 import adminai3 from "@/src/public/adminai (3).png";
 import adminai4 from "@/src/public/adminai (4).png";
+import receipt from "@/src/public/receipt (41).png";
+import receipt2 from "@/src/public/receipt (2).png";
+import receipt3 from "@/src/public/receipt2 (3).png";
+import receipt4 from "@/src/public/receipt (4).png";
 
 export const projects = [
   {
@@ -18,7 +22,7 @@ export const projects = [
     images: [adminai, adminai2, adminai3, adminai4],
     description:
       "A high-impact automation tool using Gemini AI to digitize manual workflows. It automates data extraction from invoices and synchronizes inventory, reducing manual administrative tasks by up to 80%.",
-    tags: ["Gemini AI", "Next.js", "Supabase", "OCR Automation"],
+    tags: ["Gemini AI", "Next.js", "Supabase", "Axios"],
     liveDemo: "https://adminaiassistant.vercel.app/",
     sourceCode: {
       frontEnd: "https://github.com/hanif411/adminaiassistant",
@@ -30,7 +34,7 @@ export const projects = [
     images: [circle, circle2, circle3, circle4],
     description:
       "A high-performance social networking platform engineered for speed. Implements Redis for optimized thread caching and Socket.io to deliver zero-latency real-time user interactions.",
-    tags: ["Redis", "Socket.io", "React.js", "Node.js", "Prisma"],
+    tags: ["Express", "Redis", "Socket.io", "React.js", "Node.js", "Prisma"],
     liveDemo: "https://circle-fe.vercel.app/",
     sourceCode: {
       frontEnd: "https://github.com/hanif411/circle-fe",
@@ -43,11 +47,23 @@ export const projects = [
     images: [ecommerce, ecommerce2, ecommerce3, ecommerce4],
     description:
       "A full-scale food e-commerce platform integrated with Midtrans Payment Gateway. Features automated order status updates via secure Webhooks and a streamlined checkout experience.",
-    tags: ["Midtrans", "Next.js", "MongoDB", "Zustand"],
+    tags: ["Express", "Midtrans", "Next.js", "MongoDB", "Zustand"],
     liveDemo: "https://ecommerce-food-sigma.vercel.app/",
     sourceCode: {
       frontEnd: "https://github.com/hanif411/Ecommerce-food",
       backEnd: "https://github.com/hanif411/BE-ecommerce",
+    },
+  },
+  {
+    title: "Smart Receipt Tracker",
+    tag: "Personal Finance",
+    images: [receipt, receipt2, receipt3, receipt4],
+    description:
+      "A comprehensive financial management dashboard designed to track personal expenses through receipt management. It features detailed transaction logs,and spending analytics.",
+    tags: ["Next.js", "TypeScript", "PostgreSQL", "Prisma", "Tailwind CSS"],
+    liveDemo: "https://receipttracker-mu.vercel.app/",
+    sourceCode: {
+      frontEnd: "https://github.com/hanif411/receipttracker",
     },
   },
 ];
